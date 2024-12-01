fbd5630f68fd0af170f05dc4485606254edc0db6
kat-in <newkatelife@gmail.com>
2024-12-01 05:24:40 +0300

    fix: ошибки линтера

--- src/games/brain-calc.js
+++ src/games/brain-calc.js
@@ -1,4 +1,3 @@
-import { evaluate } from 'mathjs';
 import mainLogic from '../index.js';
 import getNumber from '../getNumber.js';
 import getSign from '../getSign.js';
@@ -8,7 +7,7 @@ const description = 'What is the result of the expression?';
 
 const getGameData = () => {
   const question = `${getNumber(100)} ${getSign()} ${getNumber(100)}`;
-  const result = evaluate(question);
+  const result = eval(question);
   const correctAnswer = result.toString();
   return [question, correctAnswer];
 };
