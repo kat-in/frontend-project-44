cf00f4ae7b2f7ba9167e3a45407362bbad11f545
kat-in <newkatelife@gmail.com>
2024-12-01 04:32:39 +0300

    fix: brain-gcd question

--- src/games/brain-gcd.js
+++ src/games/brain-gcd.js
@@ -7,7 +7,7 @@ const description = 'Find the greatest common divisor of given numbers.';
 const getGameData = () => {
   const number1 = getNumber(100);
   const number2 = getNumber(100);
-  const question = (`${number1}  ${number2}`).toString();
+  const question = `${number1} ${number2}`;
   const result = getGcd(number1, number2);
   const correctAnswer = result.toString();
   return [question, correctAnswer];
