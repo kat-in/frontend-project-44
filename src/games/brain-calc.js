e707c784758861a486cd7178162c32773978027a
kat-in <newkatelife@gmail.com>
2024-12-01 05:43:09 +0300

    fix: ошибки линтера

--- src/games/brain-calc.js
+++ src/games/brain-calc.js
@@ -1,3 +1,4 @@
+import { evaluate } from 'mathjs'
 import mainLogic from '../index.js';
 import getNumber from '../getNumber.js';
 import getSign from '../getSign.js';
@@ -6,8 +7,7 @@ const description = 'What is the result of the expression?';
 
 const getGameData = () => {
   const question = `${getNumber(100)} ${getSign()} ${getNumber(100)}`;
-  // eslint-disable-line no-eval
-  const result = eval(question);
+  const result = evaluate(question);
   const correctAnswer = result.toString();
   return [question, correctAnswer];
 };
