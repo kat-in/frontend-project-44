486405e52d673f34b7246ff1a26e491f141e3323
kat-in <newkatelife@gmail.com>
2024-12-01 04:09:23 +0300

    test

--- src/index.js
+++ src/index.js
@@ -15,7 +15,7 @@ const mainLogic = (getGameData, description) => {
       console.log('Correct!');
     } else {
        console.log(`'${answer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.`);
-       console.log(`Let's try again!`);
+       console.log(`Let's try again!!!!`);
        return;
       }
   }
